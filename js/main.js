2fb7263917cfbb8e12d607f9c7a227b0dead08b9
James Newman <jamesmnewman1@gmail.com>
2016-01-07 23:54:38 -0800

    to affix nav

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
-  $(function() {
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+$(function() {
+  $('.page-scroll a').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+  });
 }); 
 
+function affixNav() {
+	if ($(window).width() > 991) {
+		$('nav').affix();
+	}
+} 
